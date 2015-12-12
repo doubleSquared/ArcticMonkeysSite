f7105c315136ab300cba089b7a4059162245708d
Athanasios Lagopoulos <sakrifor@gmail.com>
2015-12-12 16:31:45 +0200

    new pages added, fix on css.

---
+++ quiz.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Arctic Monkeys - Quiz</title>
+    <meta charset="utf-8"/>
+    <meta name="description" content="Arctic Monkeys' fan quiz"/>
+    <link rel="icon" type="image/ico" href="images/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css"/>
+</head>
+<body>
+<div id="wrapper">
+    <header>
+        <img class="logo" src="images/logo.png" alt="Arctic Monkeys"/>
+        <div id="menu">
+            <nav>
+                <ul>
+                    <li id="menuHome"><a href="index.html"> Home</a></li>
+                    <li id="menuNews"><a href="news.html">News</a></li>
+                    <li id="menuMedia"><a href="media.html">Media</a></li>
+                    <li id="menuDiscography"><a href="discography.html">Discography</a></li>
+                    <li id="menuBio"><a href="bio.html">Bio</a></li>
+                    <li id="comments"><a href="comments.php">Comments</a> </li>
+                    <li id="menuQuiz" class="active">Quiz</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</div>
+
+</body>
+    <footer><small>Copyright &copy; 2015 - <a href="mailto:lathanag@csd.auth.gr">Athanasios Lagopoulos</a></small></footer>
+</html>
