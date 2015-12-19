c8c248d0dd0d7fb95164ea41c486918abfa35510
Athanasios Lagopoulos <sakrifor@gmail.com>
2015-12-19 19:22:42 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	comments.php
    #	connection.php
     allaksa to diko moy connection

--- connection.php
+++ connection.php
@@ -1,18 +1,5 @@
 <?php
-<<<<<<< HEAD
-//Connecting to database file
 
-$host = "localhost";
-$user="user_q";
-$password ="5PZAyCjMUXHT3sHT";
-$db="quiz";
-
-$link= @mysqli_connect($host,$user,$password,$db);
-
-if(!$link){
-   echo "Problem connecting to the database, please try again later";
-}
-=======
 	$link = @mysqli_connect("localhost","syfantid","3Bm2BsFilms10%", "syfantid_");
 
 	if (!$link) {
@@ -21,4 +8,4 @@ if(!$link){
 		exit(); 
 	}
 ?>
->>>>>>> origin/master
+
